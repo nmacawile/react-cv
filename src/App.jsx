508a9c88da700d0be94ce208dddc9f2ca8f99d7a
Normar Macawile <nmacawile@gmail.com>
2023-09-19 02:33:57 +0000

    Install TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
 
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>Count is {count}</button>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => setCount(count + 1)}>Count is {count}</button>
     </>
   );
 }
