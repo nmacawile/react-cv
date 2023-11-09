e1bce5abddec43ac4ed6c1e60e748ebc69c5ca20
Normar Macawile <nmacawile@gmail.com>
2023-11-09 17:28:59 +0000

    Refactor ToggleButton click handler function

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -3,10 +3,6 @@ import { useState, useEffect } from "react";
 export function DarkModeToggle() {
   const [darkMode, setDarkMode] = useState(false);
 
-  const handleCheckboxChange = () => {
-    setDarkMode(!darkMode);
-  };
-
   useEffect(() => {
     if (
       localStorage.theme === "dark" ||
@@ -36,7 +32,7 @@ export function DarkModeToggle() {
             name="dark-mode-toggle"
             className="sr-only peer"
             checked={darkMode}
-            onChange={handleCheckboxChange}
+            onChange={() => setDarkMode(!darkMode)}
           />
           <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-amber-300 dark:peer-focus:ring-amber-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-amber-600"></div>
           <span className="ml-3 text-sm font-medium text-black dark:text-gray-300">
