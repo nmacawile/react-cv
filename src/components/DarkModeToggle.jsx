7438a7b9a5bc2f5acd5dc8f2878afb4c2beb57b3
Normar Macawile <nmacawile@gmail.com>
2023-11-09 17:25:06 +0000

    Fix dark mode toggle off state in dark mode while in non-strict mode

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,24 +1,31 @@
 import { useState, useEffect } from "react";
 
 export function DarkModeToggle() {
-  const darkMode = localStorage.theme === "dark";
-  const [isChecked, setIsChecked] = useState(darkMode);
+  const [darkMode, setDarkMode] = useState(false);
 
   const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
+    setDarkMode(!darkMode);
+  };
 
-    if (!isChecked) {
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setDarkMode(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (darkMode) {
       document.documentElement.classList.add("dark");
       localStorage.theme = "dark";
     } else {
       document.documentElement.classList.remove("dark");
       localStorage.theme = "light";
     }
-  };
-
-  useEffect(() => {
-    setIsChecked(localStorage.theme === "dark");
-  }, []);
+  }, [darkMode]);
 
   return (
     <>
@@ -28,7 +35,7 @@ export function DarkModeToggle() {
             type="checkbox"
             name="dark-mode-toggle"
             className="sr-only peer"
-            checked={isChecked}
+            checked={darkMode}
             onChange={handleCheckboxChange}
           />
           <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-amber-300 dark:peer-focus:ring-amber-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-amber-600"></div>
